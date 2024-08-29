70c66b1119fb897df46cce00ffc653007b4a60bd
Jackie <Jackie.Pan@studentfr.ch>
2024-08-29 11:44:36 +0200

    exercice 4

---
+++ exercices/exercice04/EchangeValeurs.java
@@ -0,0 +1,12 @@
+package exercices.exercice04;
+
+public class EchangeValeurs {
+    public static void main(String[] args) {
+        int variable1 = 1;
+        int variable2 = 2;
+        
+
+
+    
+}
+}
