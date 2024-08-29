44293b4ffbf4377ae073404d75372ee133b02013
Jackie <Jackie.Pan@studentfr.ch>
2024-08-29 11:02:22 +0200

    exercice 2

---
+++ exercices/exercice03/TypeDeBase.java
@@ -0,0 +1,13 @@
+package exercices.exercice03;
+
+public class TypeDeBase {
+    public static void main(String[] args) {
+        int age = 15;
+        System.out.println(age);
+        String name = ("Jackie");
+        System.out.println(name);
+        System.out.println("Je m'appelle " + name +" et j'ai " + age + " ans");
+
+
+    }
+}
