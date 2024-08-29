3b249c343a660b0e2d30de12aad2c3e48a35a431
Jackie <Jackie.Pan@studentfr.ch>
2024-08-29 11:11:33 +0200

    exercice 3

--- exercices/exercice03/TypeDeBase.java
+++ exercices/exercice03/TypeDeBase.java
@@ -7,6 +7,7 @@ public class TypeDeBase {
         String name = ("Jackie");
         System.out.println(name);
         System.out.println("Je m'appelle " + name +" et j'ai " + age + " ans");
+        System.out.println("Dans 10 and j'aurais " + (age+10));
 
 
     }
