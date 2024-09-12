1c91c566127256cda21539b1624d1518b3fd51b4
Jackie <Jackie.Pan@studentfr.ch>
2024-09-12 09:11:41 +0200

    exercice boucles

---
+++ exercices/exercice08/ExerciceBoucles1.java
@@ -0,0 +1,11 @@
+package exercice08;
+
+public class ExerciceBoucles1 {
+    public static void main(String[] args) {
+        for(int i=1; i<6; i++){
+            System.out.println("i: "+ i);
+        }
+    }
+
+    
+}
---
+++ exercices/exercice08/exerciceBoucles2.java
@@ -0,0 +1,19 @@
+package exercice08;
+
+public class exerciceBoucles2 {
+    public static void main(String[] args) {
+        int nbr=0;
+        boolean uneCondition=true;
+        while (uneCondition=true){
+            System.out.println(nbr+=1);
+            if (nbr==5){
+                uneCondition=false;
+                if (uneCondition=false);{
+                    break;
+                }
+            }
+        }
+    }
+
+    
+}
