4b20fafb5bac53d6dccbe11cd0445adc7fdd6051
Jackie <Jackie.Pan@studentfr.ch>
2024-09-12 11:44:43 +0200

    devoir 2

--- devoirs/devoir02/devoir02.java
+++ devoirs/devoir02/devoir02.java
@@ -1,20 +1,34 @@
 package devoir02;
 
+import java.util.Random;
+
 public class devoir02 {
+    public static final int CAPACITE_RESERVOIR_A = 3;
+    public static final int CAPACITE_RESERVOIR_B = 5;
+    
     public static void main(String[] args) {
-        int CAPACITE_RESERVOIR_A = 3;
-        int CAPACITE_RESERVOIR_B = 5;
-        int réservoirA = 0;
-        int réservoirB = 0;
-        int max=3; int min=0;
-        int remplissage = 0;
-        remplissage = min +(int)(Math.random()*((max|min)));
-        do{
-            System.out.println("remplissage du réservoirA");
-            System.out.println("Le réservoirA: "+ (réservoirA+remplissage) + ", le réservoirB: "+ réservoirB);
+        int reservoirA = 0;
+        int reservoirB = 0;
+        Random random = new Random();
+        int remplissage = random.nextInt(CAPACITE_RESERVOIR_A + CAPACITE_RESERVOIR_B + 1);
+
+        System.out.println("Nombre de litres à remplir : " + remplissage);
+
+        while (reservoirA + reservoirB < remplissage) {
+            if (reservoirA != CAPACITE_RESERVOIR_A) {
+                reservoirA++;
+                System.out.println("Réservoir A : " + reservoirA + " litre(s), Réservoir B : " + reservoirB + " litre(s)");
+            } else if (reservoirB != CAPACITE_RESERVOIR_B) {
+                // Remplir le réservoir B une fois que le réservoir A est plein
+                reservoirB++;
+                System.out.println("Réservoir A : " + reservoirA + " litre(s), Réservoir B : " + reservoirB + " litre(s)");
+            }
         }
-        while(réservoirA>=CAPACITE_RESERVOIR_A);
-        
+
+        // Affichage final
+        System.out.println("Remplissage terminé !");
+        System.out.println("Réservoir A : " + reservoirA + " litre(s)");
+        System.out.println("Réservoir B : " + reservoirB + " litre(s)");
     }
     
 }
