89c7852caeeb7f2376309860da651306a50767b4
Jackie <Jackie.Pan@studentfr.ch>
2024-09-12 09:41:36 +0200

    ex

--- exercices/exercice08/exerciceBoucles3.java
+++ exercices/exercice08/exerciceBoucles3.java
@@ -2,11 +2,12 @@ package exercice08;
 
 public class exerciceBoucles3 {
     public static void main(String[] args) {
-        int nbr=0;
-        boolean condition=true;
+        int nbr=1;
         do{
-            
+            System.out.println(nbr);
+            nbr++;
         }
+        while(nbr<=5);
     }
     
 }
