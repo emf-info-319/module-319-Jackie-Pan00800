0571bafd08ab97eb2c75badc72aa1407dae4747d
Jackie <Jackie.Pan@studentfr.ch>
2024-09-19 11:43:23 +0200

    ex

---
+++ exercices/exercice09/ExercicesTableau4.java
@@ -0,0 +1,14 @@
+package exercice09;
+
+public class ExercicesTableau4 {
+    public static void main(String[] args) {
+        int nombreDébut=17;
+        int[] tableauNBR2;
+        tableauNBR2 = new int [] {nombreDébut,nombreDébut+1,
+        nombreDébut+2,nombreDébut+3,nombreDébut+4,nombreDébut+5};
+        for (int i = 0; i < tableauNBR2.length; i++) {
+            System.out.println(tableauNBR2[i]);
+        }
+    }
+    
+}
