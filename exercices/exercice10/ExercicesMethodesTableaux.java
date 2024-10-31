43393b0909885eef983e4129ca7c0e387594bcdb
Jackie <Jackie.Pan@studentfr.ch>
2024-10-31 11:26:33 +0100

    ex

---
+++ exercices/exercice10/ExercicesMethodesTableaux.java
@@ -0,0 +1,16 @@
+package exercice10;
+
+public class ExercicesMethodesTableaux {
+    public static void main(String[] args) {
+        int taille=5;
+        tableau1(taille);
+    }
+    public static int[] tableau1(int taille){
+        int[] leTableau; 
+        leTableau= new int[taille];
+        return leTableau;
+    }
+    public static int[] tableau2(){
+        int dansTBL= int tableau[5];
+    }
+}
