c87e457abe33c8e090616aea6341b3a9ff782da2
Jackie.Pan <Jackie.Pan@studentfr.ch>
2024-11-21 11:11:14 +0100

    rev

---
+++ exercices/rp.java
@@ -0,0 +1,12 @@
+public class rp {
+    public static int variable = 89;
+    public static void main(String[] args) {
+        //Ceci est un commentaire.
+        System.out.println("hello world") ;
+        
+
+
+        }
+        
+    
+}
---
+++ revision2/TournoisJackiePan.java
@@ -0,0 +1,9 @@
+package revision2;
+
+public class TournoisJackiePan {
+    public static String[] EQUIPES=new String[]{"FC Richemont","FC Central","FC Schoenberg","FC Beauregard"}
+    public static void main(String[] args) {
+        
+    }
+    
+}
